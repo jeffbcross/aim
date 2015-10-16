0fd7fa92a461d24741019e630086456f8ae86312
Ben Lesh <blesh@netflix.com>
2015-10-15 17:16:24 -0700

    docs: add rx docs to typeahead rx-y stuff

--- src/components/typeahead/typeahead.ts
+++ src/components/typeahead/typeahead.ts
@@ -20,18 +20,30 @@ import {RxPipe} from '../../services/rx-pipe/rx-pipe';
 export class TypeAhead {
   @Output('selected') selected = new EventEmitter();
   clear = new EventEmitter();
+
   ticker = new Control();
 
   tickers: Observable<any[]>;
+
   constructor(http:Http) {
+    // get a stream of changes from the tickers input
     this.tickers = Observable.from((<EventEmitter>this.ticker.valueChanges).toRx())
+      // wait for a pause in typing of 200ms then emit the last value
       .debounceTime(200)
+      // only accept values that don't repeat themselves
       .distinctUntilChanged()
+      // map that to an observable HTTP request and switch to that
+      // observable. That means unsubscribing from any previous HTTP request
+      // (cancelling it), and subscribing to the newly returned on here.
       .switchMap(val => {
         return http.request(`http://localhost:3000/stocks?symbol=${val}`)
       }, (val:string, res:Response) => res.json())
+      // send an empty array to tickers whenever clear emits by
+      // merging in a the stream of clear events mapped to an
+      // empty array.
       .merge(this.clear.toRx().mapTo([]));
   }
+
   onSelect(ticker){
     this.selected.next(ticker);
     this.clear.next('');
