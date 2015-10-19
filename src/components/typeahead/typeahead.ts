82f60ce404b1a86898544b5bf4108a6cc2994234
Jeff Cross <middlefloor@gmail.com>
2015-10-19 15:33:58 +0100

    refactor(typeahead): use TickerFetch service to make http requests

--- src/components/typeahead/typeahead.ts
+++ src/components/typeahead/typeahead.ts
@@ -2,6 +2,7 @@ import {Component, Control, EventEmitter, FORM_DIRECTIVES, NgControl, NgFor, Cha
 import {Http, Response} from 'angular2/http';
 import {Observable, Subject} from '@reactivex/rxjs';
 import {RxPipe} from '../../services/rx-pipe/rx-pipe';
+import {TickerFetch} from '../../services/ticker_fetch/ticker_fetch';
 
 @Component({
   selector: 'typeahead',
@@ -15,7 +16,8 @@ import {RxPipe} from '../../services/rx-pipe/rx-pipe';
   `,
   directives: [FORM_DIRECTIVES, NgFor],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  pipes: [RxPipe]
+  pipes: [RxPipe],
+  providers: [TickerFetch]
 })
 export class TypeAhead {
   @Output('selected') selected = new EventEmitter();
@@ -25,19 +27,18 @@ export class TypeAhead {
 
   tickers: Observable<any[]>;
 
-  constructor(http:Http) {
+  constructor(http:Http, tickerFetch:TickerFetch) {
     // get a stream of changes from the tickers input
     this.tickers = Observable.from((<EventEmitter>this.ticker.valueChanges).toRx())
       // wait for a pause in typing of 200ms then emit the last value
       .debounceTime(200)
       // only accept values that don't repeat themselves
       .distinctUntilChanged()
-      // map that to an observable HTTP request and switch to that
+      // map that to an observable HTTP request, using the TickerFetch
+      // service and switch to that
       // observable. That means unsubscribing from any previous HTTP request
       // (cancelling it), and subscribing to the newly returned on here.
-      .switchMap(val => {
-        return http.request(`http://localhost:3000/stocks?symbol=${val}`)
-      }, (val:string, res:Response) => res.json())
+      .switchMap((val:string) => tickerFetch.fetch(val))
       // send an empty array to tickers whenever clear emits by
       // merging in a the stream of clear events mapped to an
       // empty array.
