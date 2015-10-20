5b314c28f6c7733ac0eb4025d9cb89e571007362
Jeff Cross <middlefloor@gmail.com>
2015-10-20 11:49:05 +0100

    refactor(typeahead): rename "ticker" property to "searchText"

--- src/components/typeahead/typeahead.ts
+++ src/components/typeahead/typeahead.ts
@@ -13,7 +13,7 @@ import {TickerLoader} from '../../services/ticker_loader/ticker_loader';
         type="text"
         class="form-control"
         placeholder="Search for Symbol..."
-        #symbol [ng-form-control]="ticker" aria-describedby="symbol-input">
+        #symbol [ng-form-control]="searchText" aria-describedby="symbol-input">
     </div>
     <table class="table table-striped">
       <tbody>
@@ -34,13 +34,13 @@ export class TypeAhead {
   @Output('selected') selected = new EventEmitter();
   clear = new EventEmitter();
 
-  ticker = new Control();
+  searchText = new Control();
 
   tickers: Observable<any[]>;
 
   constructor(http:Http, tickerLoader:TickerLoader) {
     // get a stream of changes from the tickers input
-    this.tickers = Observable.from((<EventEmitter>this.ticker.valueChanges).toRx())
+    this.tickers = Observable.from((<EventEmitter>this.searchText.valueChanges).toRx())
       // wait for a pause in typing of 200ms then emit the last value
       .debounceTime(200)
       // only accept values that don't repeat themselves
